47cf29327908e6f9ba98ed1120ad691550c49e71
Prodi <programer972@gmail.com>
2026-06-17 12:54:14 +0300

    writing a route for /webhooks/clerk

--- backend/index.ts
+++ backend/index.ts
@@ -3,6 +3,7 @@ import { serve } from "@hono/node-server";
 import { cors } from "hono/cors";
 import { clerkMiddleware, getAuth } from "@clerk/hono";
 import "dotenv/config";
+import { bodyLimit } from "hono/body-limit";
 
 const app = new Hono();
 
@@ -15,6 +16,20 @@ app.use(
 );
 
 app.use("*", clerkMiddleware());
+
+const sizeLimiter = bodyLimit({
+  maxSize: 1 * 1024 * 1024,
+  onError: (c) => c.text("Payload Too Large", 413),
+});
+
+app.post("/webhooks/clerk", sizeLimiter, async (c) => {
+  const rawBody = await c.req.text();
+  await clerkWebhookHandler(c, rawBody);
+
+  return c.json({ success: true });
+});
+
+
 
 app.get("/", (c) => {
   return c.text("Hello Dimon!!!");
